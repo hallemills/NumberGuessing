563468b0aa09960a7b14c84543d3a4fc78ca21b2
halle <halle.mills.2022@student.lhschools.org>
2019-10-25 09:36:12 -0400

    made an else if statement

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -8,7 +8,13 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
+        double number;
+
+        System.out.println("Pick a number");
+        number = keyboard.nextDouble();
+
         int pickedNumber;
+
 
         for (int h = 0; h < 10; h++) {
             pickedNumber = (int) (Math.random() * 5) + 4;
@@ -17,7 +23,25 @@ public class Main {
 
             do {
                 System.out.println("I am inside the do while loop");
-            }while (0 < 10);
+            } while (0 < 10);
+
+
+            if (10 >= number && number > 0) {
+                System.out.println("You win");
+            } else if (20 >= number && number > 10) {
+                System.out.println("You lose");
+            }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
