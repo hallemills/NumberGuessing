b66add7cf6df576464842eeee5ca20205fdba8ae
halle <halle.mills.2022@student.lhschools.org>
2019-10-28 09:25:38 -0400

    deleted the else if statement

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,49 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner keyboard;
-        keyboard = new Scanner(System.in);
 
-        double number;
+        int PickedNumber;
+        PickedNumber = (int) (Math.random()*19);
+        System.out.println(PickedNumber);
 
-        System.out.println("Pick a number");
-        number = keyboard.nextDouble();
+        //}
+    }
 
-        int pickedNumber;
-
-
-        for (int h = 0; h < 10; h++) {
-            pickedNumber = (int) (Math.random() * 5) + 4;
-            System.out.println(pickedNumber);
-
-
-            do {
-                System.out.println("I am inside the do while loop");
-            } while (0 < 10);
-
-
-            if (10 >= number && number > 0) {
-                System.out.println("You win");
-            } else if (20 >= number && number > 10) {
-                System.out.println("You lose");
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
